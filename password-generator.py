f7e39812a9a28c92a8119099663b21a7240b712f
SupremeERG <ethang717163@icloud.com>
2021-05-14 00:11:24 -0700

    Fixed password-generator

---
+++ password-generator.py
@@ -0,0 +1,41 @@
+from random import randint
+small = "abcdefghijklmnopqrstuvwyxz"
+large = small.upper()
+numbers = "1234567890"
+special = "!!@#$%^&*()_"
+
+print("If anything other than 'hard', 'medium' or 'low' is entered then the difficulty will automatically be hard")
+
+crack_difficulty = input("How difficult would you like this password to be to crack (hard, medium, low) ? ")
+if crack_difficulty.lower() == "hard":
+    password_stuff = small + numbers + special + large
+elif crack_difficulty.lower() == "medium":
+    password_stuff = small + "_" + large + numbers
+elif crack_difficulty.lower() == "low":
+    password_stuff = small + numbers
+
+password_length = int(input("Enter the length of the password >> "))
+#password = ""
+#length = 0
+
+if password_length < 5 or password_length > 80:
+    print("Minimum password length must be at least 5 and no more than 80 ")
+else:
+    def redo(times):
+        password = ""
+        length = 0
+        if times == 0:
+            print("\nFeeling insecure about your new password? Test it on https://www.my1login.com/resources/password-strength-test/")
+            exit()
+        while length<password_length:
+            password = password + password_stuff[randint(0, len(password_stuff)-1)]
+            length += 1
+        print(f"Generated password: {password}")
+        redo(times - 1)
+    
+    generate_amt = int(input("How many passwords would you like to generate >> "))
+    if generate_amt > 5 or generate_amt < 1:
+        print("You must generate 1-5 passwords")
+    else:
+        print("\tGENERATED PASSWORDS\t\n-------------------------------------")
+        redo(generate_amt)
